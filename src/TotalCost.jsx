5262ef790529e0d366df24dada2b60b612820a03
RPG <rpg@example.com>
2026-05-26 12:44:25 +0200

    Task 6: Display selected items in a table Display selected event items in details table

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -2,26 +2,29 @@ import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
+    const grandTotal = totalCosts.venue + totalCosts.av + totalCosts.meals;
 
-  return (
-    <div className="pricing-app">
-      <div className="display_box">
-        <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
-        </div>
-        <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
-         
-            <div>
-             
+    return (
+        <div className="pricing-app">
+            <div className="display_box">
+                <div className="header">
+                    <p className="preheading">
+                        <h3>Total cost for the event</h3>
+                    </p>
+                </div>
+
+                <div>
+                    <h2 id="pre_fee_cost_display" className="price">
+                        ${grandTotal}
+                    </h2>
+
+                    <div>
+                        <ItemsDisplay />
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TotalCost;
